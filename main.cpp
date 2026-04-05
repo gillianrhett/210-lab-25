19159b5475ae5407a44ff9c7d4ace2af7a1aeac9
gillianrhett <gillianrhett@gmail.com>
2026-04-04 18:46:06 -0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include <chrono>
 using namespace std;
 
+
+
 int main() {
-    
+
+    /*
+    The first race is READING. The challenge is to read the 20,000 data elements into each of these data structures: vector, list, set
+    The second race is SORTING. Sort the vector and sort the list. Set the set's value to -1 since a set is already sorted.
+    The third race is INSERTING. Insert the value "TESTCODE" into the middle of the vector or the middle of the list, or into the set
+    The fourth race is DELETING. Delete the middle-ish element of the vector, the list, or the set
+    */
 
     return 0;
 }
