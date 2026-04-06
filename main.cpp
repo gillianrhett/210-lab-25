eba7a1cac9c462bfcdd40a479a023f82d6675fa0
gillianrhett <gillianrhett@gmail.com>
2026-04-05 20:59:49 -0700

    done adding timing; checking for any errors

--- main.cpp
+++ main.cpp
@@ -38,20 +38,36 @@ int main() {
     list<string> myList;
     inFile.clear(); 
     inFile.seekg(0, ios::beg);
+    // start timing
+    start = high_resolution_clock::now();
+    // read from file into list
     while (!inFile.eof()){
         getline(inFile, code);
         myList.push_back(code);
     }
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    l_read = duration.count();
 
     // create and populate a set from the file
     int s_read = 0; // time in nanoseconds it takes to read the data into the set
     set<string> mySet;
     inFile.clear(); 
     inFile.seekg(0, ios::beg);
+    // start timing
+    start = high_resolution_clock::now();
+    // read from file into set
     while (!inFile.eof()){
         getline(inFile, code);
         mySet.insert(code);
     }
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    s_read = duration.count();
 
     // done reading from file
     inFile.close();
@@ -59,11 +75,27 @@ int main() {
     // TASK 2: SORT
     // sort the vector
     int v_sort = 0; // time in nanoseconds it takes to sort the vector;
+    // start timing
+    start = high_resolution_clock::now();
+    // sort vector
     sort(myVector.begin(), myVector.end());
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    v_sort = duration.count();
 
     // sort the list
     int l_sort = 0; // time in nanoseconds it takes to sort the list
+    // start timing
+    start = high_resolution_clock::now();
+    // sort list
     myList.sort();
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    l_sort = duration.count();
 
     // the set is already sorted
     int s_sort = -1;
@@ -71,40 +103,88 @@ int main() {
     // TASK 3: INSERT
     // insert "TESTCODE" in the middle of the vector
     int v_insert = 0; // time in nanoseconds to insert in vector
+    // start timing
+    start = high_resolution_clock::now();
+    // insert item in the middle
     vector<string>::iterator v_mid = myVector.begin() + (myVector.size() / 2); // points to the middle item
     myVector.emplace(v_mid, "TESTCODE");
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    v_insert = duration.count();
 
     // insert "TESTCODE" in the middle of the list
     int l_insert = 0; // time in nanoseconds to insert in list
+    // start timing
+    start = high_resolution_clock::now();
+    // insert item in the middle
     // list<string>::iterator l_mid = myList.begin() + (myList.size() / 2); // not working
     list<string>::iterator l_mid = myList.begin();
     // make l_mid point to middle item
     for(int i = 0; i < myList.size() / 2; ++i)
         advance(l_mid, 1);
     myList.emplace(l_mid, "TESTCODE");
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    l_insert = duration.count();
 
     // insert "TESTCODE" in the set
     int s_insert = 0; // time in nanoseconds to insert in set
+    // start timing
+    start = high_resolution_clock::now();
+    // insert item in the middle
     mySet.insert("TESTCODE");
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    s_insert = duration.count();
 
     // TEST 4: DELETE
     // delete the middle item from the vector
     int v_delete = 0; // time in nanoseconds to delete middle item from vector
+    // start timing
+    start = high_resolution_clock::now();
+    // delete item from the middle
     // v_mid still points to the middle one
     myVector.erase(v_mid);
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    v_delete = duration.count();
 
     // delete the middle item from the list
     int l_delete = 0; // time in nanoseconds to delete the middle item from list
+    // start timing
+    start = high_resolution_clock::now();
+    // delete item from the middle
     myList.erase(l_mid);
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    l_delete = duration.count();
 
     // delete the middle item from the set
     int s_delete = 0; // time in nanoseconds to delete the middle item from set
+    // start timing
+    start = high_resolution_clock::now();
+    // delete item from the middle
     // now we do need an iterator to the middle item
     set<string>::iterator s_mid = mySet.begin();
     // make s_mid point to middle item
     for(int i = 0; i < mySet.size() / 2; ++i)
         advance(s_mid, 1);
     mySet.erase(s_mid);
+    // end timing
+    end = high_resolution_clock::now();
+    // calculate duration
+    duration = duration_cast<nanoseconds>(end - start);
+    s_delete = duration.count();
 
     // display the results
     cout << setw(10) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
